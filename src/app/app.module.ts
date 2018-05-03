aa5e3f582803d143242bfd166ea06e657e6fd7dc
Yash <yasodha@rjtcompuquest.com>
2018-05-03 16:39:45 -0500

    Todo components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoformComponent } from './todoform/todoform.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import {TodoService} from './todo.service';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoComponent,
+    TodoformComponent,
+    TodolistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
